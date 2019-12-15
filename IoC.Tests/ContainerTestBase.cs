e64ddb12798082efb6bc60d3046b64a67520b22c
Milos <cubrilo.milos@gmail.com>
2019-12-15 04:10:33 +0100

    initial commit

---
+++ IoC.Tests/ContainerTestBase.cs
@@ -0,0 +1,36 @@
+﻿using IoCContainer;
+using NUnit.Framework;
+using ShouldBeAssertions.Core;
+
+namespace IoC.Tests
+{
+    public class ContainerTestBase
+    {
+        protected Container Container;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            Container = new Container();
+        }
+
+        [TearDown]
+        public void AfterEach()
+        {
+            Container = null;
+        }
+    }
+
+    [TestFixture]
+    public class Container_GetInstance : ContainerTestBase
+    {
+        [Test]
+        public void CreateInstanceWithParams()
+        {
+            var subject = (A) Container.GetInstance(typeof(A));
+            Assert.AreEqual(typeof(A), subject.GetType());
+        }
+    }
+    class A { }
+
+}
---
+++ IoCContainer/Container.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IoCContainer
+{
+    public class Container
+    {
+        public object GetInstance(Type type)
+        {
+            return Activator.CreateInstance(type);
+        }
+    }
+}
