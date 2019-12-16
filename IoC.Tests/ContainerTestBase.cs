59ad177900cd00824413f80fbc09f5f1db880950
Milos <cubrilo.milos@gmail.com>
2019-12-16 20:44:40 +0100

    Create instance with params

--- IoC.Tests/ContainerTestBase.cs
+++ IoC.Tests/ContainerTestBase.cs
@@ -25,12 +25,29 @@ namespace IoC.Tests
     public class Container_GetInstance : ContainerTestBase
     {
         [Test]
-        public void CreateInstanceWithParams()
+        public void CreateInstanceWithNoParams()
         {
             var subject = (A) Container.GetInstance(typeof(A));
             Assert.AreEqual(typeof(A), subject.GetType());
         }
+
+        [Test]
+        public void CreateInstanceWithParams()
+        {
+            var subject = (B)Container.GetInstance(typeof(B));
+            Assert.AreEqual(typeof(A), subject.A.GetType());
+        }
     }
     class A { }
+    class B 
+    { 
+        public A A { get; }
 
+        public B() { }
+
+        public B(A a)
+        {
+            A = a;
+        }
+    }
 }
--- IoCContainer/Container.cs
+++ IoCContainer/Container.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace IoCContainer
@@ -8,7 +9,13 @@ namespace IoCContainer
     {
         public object GetInstance(Type type)
         {
-            return Activator.CreateInstance(type);
+            var constructor = type.GetConstructors()
+                .OrderByDescending(c => c.GetParameters().Length).First();
+
+            var args = constructor.GetParameters()
+                .Select(param => GetInstance(param.ParameterType)).ToArray();
+
+            return Activator.CreateInstance(type, args);
         }
     }
 }
